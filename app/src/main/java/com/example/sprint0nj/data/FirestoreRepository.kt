3bbeef2ad05fbf26de50b0e931b87ad109b89272
Davis Owen <davis.owen752@myci.csuci.edu>
2025-03-05 15:26:56 -0800

    Adding Firestore Repo and Playlists template files

---
+++ app/src/main/java/com/example/sprint0nj/data/FirestoreRepository.kt
@@ -0,0 +1,22 @@
+package com.example.sprint0nj.data
+import com.google.firebase.firestore.FirebaseFirestore
+import com.example.sprint0nj.data.Playlists.Playlist
+
+class FirestoreRepository {
+
+    private val db = FirebaseFirestore.getInstance()
+    private val playlistsCollection = db.collection("playlists")
+
+    // Function to add a playlist to Firestore using provided playlist
+    fun addPlaylist(playlist: Playlist, callback: (Boolean, String?) -> Unit) {
+        playlistsCollection.document(playlist.id).set(playlist) // sets document w/ id "document.id"to value of playlist
+            .addOnSuccessListener {
+                callback(true, null) // If successful, continue
+            }
+            .addOnFailureListener { exception ->
+                callback(false, exception.message) // If fails, return error message and false
+            }
+    }
+
+
+}
---
+++ app/src/main/java/com/example/sprint0nj/data/Playlists.kt
@@ -0,0 +1,7 @@
+package com.example.sprint0nj.data
+
+class Playlists {
+    data class Playlist(
+        val id : String = ""
+    )
+}
